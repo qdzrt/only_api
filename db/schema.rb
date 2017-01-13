598fea622c60e88c10ba88b9ec5da71e8a968b8c
lian <itemyaimydwhg@163.com>
2017-01-13 15:21:06 +0800

    add active_model_serializers gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113065232) do
+ActiveRecord::Schema.define(version: 20170113071407) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
